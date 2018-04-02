556c7268a1b99187fdbaaf7a9d975ec0e54f99e3
pepperoni666 <peretser08@gmail.com>
2018-04-02 13:25:52 +0200

    Splash screen added

---
+++ RealFishki/RealFishki.Android/Splash.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace RealFishki.Droid
+{
+    [Activity(Label = "RealFishki", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true, Icon = "@drawable/icon")]
+    public class Splash : Activity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+        
+        async void SimulateStartup()
+        {
+            await Task.Delay(100);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
